d694cd2a287f2155a4070a6cac823bf2f883723c
hyejun <hyejunkkim228@gmail.com>
2026-09-17 22:11:42 +0900

    chore: query provider 설정 변경

--- src/_app/providers/app-providers.tsx
+++ src/_app/providers/app-providers.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { useState, type PropsWithChildren } from 'react';
+import { QueryClientProvider } from '@tanstack/react-query';
+import type { PropsWithChildren } from 'react';
 
+import { getQueryClient } from '@/shared/api';
 import { ToastViewport } from '@/shared/ui/toast';
 
 export function AppProviders({ children }: PropsWithChildren) {
-  const [queryClient] = useState(() => new QueryClient());
+  const queryClient = getQueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
